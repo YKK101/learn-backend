87aaead83466c2ad2f236f222106cdfcb0fdedae
kanya k <kanya_ykk@hotmail.com>
2017-12-25 11:30:56 +0700

    add eslint & babel & use express

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+export const {
+  NODE_PORT,
+} = process.env
--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+import express from 'express'
+
+const app = express()
+const {
+  NODE_PORT,
+} = require('./config')
+
+const nodePort = NODE_PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(nodePort, () => {
+  console.log(`App listening on port ${nodePort}`)
+})
+
